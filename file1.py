0b239e9c45972a9fd66ef4968f02286072f699f0
gavanwp <wpgavan@gmail.com>
2023-04-14 10:06:46 +0500

    python programming

---
+++ file1.py
@@ -0,0 +1,19 @@
+print("Hello world")
+print(4+23/77*23-44)
+
+
+print(55)
+#the new line character “\n” is used to create a new line.
+print("hello i am \"gavan kumar\" \n this is my friend roshan")
+print("hello",44,23, sep=">" , end="4345\n")
+print("new line ")
+
+a = " hello dear friend how are you i hope you are doing well "
+print(a[3:4])  # Prints characters starting from 3rd to 5th
+print(a[2:]) # Prints string starting from 3rd character
+print(a*2) ## Prints string two times
+print(a+"Gavan kumar") # Prints concatenated string
+ #Python List Data Type
+
+
+
---
+++ variables.py
@@ -0,0 +1,30 @@
+a = ("hello this is first varibale")
+print(a)
+b = ("i am gavan kumar wordpress developer and desinger")
+print(b)
+c = (12)
+print(c)
+d=(22)
+print(c + d)
+#This is bol type 
+e=True
+print("The type of a is " , type(e))
+
+ # complex number 
+
+a2 = complex(2 , 4)
+print(a2)
+# sequenced data type 
+list1 = ["HELLO" , 11 , 8.1 , ["This is sequenced list"]]
+print(list1) 
+# tuple1  immutable or mutable  
+# mutable mean liable to change
+#immutable mean unable to be changed.
+tuple= ("hello" , 22 , ("hello"))
+print(tuple)
+
+
+# 5 Mapped data : dict
+dict1 = {"name":"Gavan", "class":8, "age":18}
+print(dict1)
+
